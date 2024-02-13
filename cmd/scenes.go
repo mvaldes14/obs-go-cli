e078f6c3bb4cfbe77bbc3825c57ee677fe4c894b
mvaldes <mvaldes.revilla@gmail.com>
2024-02-12 22:41:21 -0600

    start/stop stream commands

--- cmd/scenes.go
+++ cmd/scenes.go
@@ -2,22 +2,21 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/andreykaipov/goobs"
 	"github.com/andreykaipov/goobs/api/requests/scenes"
 	"github.com/spf13/cobra"
 )
 
-var sceneList = []string{"Starting", "Secrets", "Coding", "Ending"}
+var sceneList = []string{"Starting", "Secrets", "Coding", "Ending","Gaming", "Talk"}
 
 func changeScene(sceneName string) {
 	var err error
-
 	client, err := goobs.New("localhost:4455")
 	if err != nil {
 		panic(err)
 	}
 	defer client.Disconnect()
-
 	n, err := client.Scenes.SetCurrentProgramScene(&scenes.SetCurrentProgramSceneParams{
 		SceneName: &sceneName,
 	})
@@ -25,6 +24,23 @@ func changeScene(sceneName string) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func manipulateStream(action string) {
+  var err error
+  client, err := goobs.New("localhost:4455")
+  if err != nil {
+    panic(err)
+  }
+  defer client.Disconnect()
+  if action == "start" {
+    _, err = client.Stream.StartStream()
+  } else if action == "stop" {
+    _, err = client.Stream.StopStream()
+  }
+  if err != nil {
+    panic(err)
+  }
 }
 
 func isStringNotInArray(target string, array []string) bool {
@@ -55,6 +71,21 @@ var ScenesCmd = &cobra.Command{
 	},
 }
 
+var StreamCmd = &cobra.Command{
+  Use: "stream",
+  Short: "Starts or stops the stream",
+  Long: `Starts or stops the stream`,
+  Run : func(cmd *cobra.Command, args []string) {
+    if len(args) < 1 {
+      fmt.Println("Please provide a valid action")
+      return
+    }
+    manipulateStream(args[0])
+  },
+}
+
+
 func init() {
 	rootCmd.AddCommand(ScenesCmd)
+	rootCmd.AddCommand(StreamCmd)
 }
